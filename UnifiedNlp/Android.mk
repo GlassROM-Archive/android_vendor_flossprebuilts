1abc57d5e76c48ce5c86760761ef36c6350c7777
anupritaisno1 <www.anuprita804@gmail.com>
2020-04-27 23:53:06 +0200

    update unifiednlp
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- UnifiedNlp/Android.mk
+++ UnifiedNlp/Android.mk
@@ -6,6 +6,15 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_REQUIRED_MODULES := unifiednlp-power-whitelist.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := unifiednlp-power-whitelist.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
